b7ce4c9d45a6a7a4b63f11c0067b41052e99ee1f
Jaiden DeChon <wailer.hideout.0l@icloud.com>
2024-10-24 00:42:13 +0000

    Improved hero pane

--- components/homepage/hero-pane.vue
+++ components/homepage/hero-pane.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hero-parent">
-        <div class="size-for-all-screens flex flex-col justify-between items-center gap-12 p-9 md:flex-row-reverse">
+        <div class="size-for-all-screens flex flex-col justify-between items-center gap-12 p-9 md:flex-row-reverse xl:px-0">
             <div class="flex justify-center w-full">
                 <img
                     src="/img/headshot.jpg"
@@ -8,27 +8,27 @@
                 >
             </div>
 
-            <div class="flex flex-col gap-12 md:gap-9 md:max-w-xl">
-                <h1 class="text-4xl font-medium hero-text">
-                    👋 Hello! I'm
-                    <span class="text-brand-blue">Jaiden</span>.
+            <div class="flex flex-col gap-12 md:gap-9">
+                <h1 class="text-4xl font-medium">
+                    👋 Hello! I'm Jaiden.
+                    <!-- <span class="bg-brand-yellow px-2 text-brand-yellow-foreground">Jaiden</span>. -->
                 </h1>
 
-                <p class="w-full text-2xl font-medium hero-text">
+                <p class="w-full text-2xl font-medium leading-10">
                     I create
-                    <span class="text-brand-blue">beautiful web-apps</span>
+                    <span class="bg-brand-yellow px-2 text-brand-yellow-foreground rounded-sm">beautiful web-apps</span>
                     with an intense focus on
-                    <span class="text-brand-blue">performance</span>
+                    <span class="bg-brand-yellow px-2 text-brand-yellow-foreground rounded-sm">performance</span>
                     and
-                    <span class="text-brand-blue">accessibility</span>.
+                    <span class="bg-brand-yellow px-2 text-brand-yellow-foreground rounded-sm">accessibility</span>.
                 </p>
 
-                <p class="w-full text-2xl font-medium hero-text">
+                <p class="w-full text-2xl font-medium leading-10">
                     When I'm not coding, I love learning about ancient history, sprituality, and consciousness.
                 </p>
 
                 <div class="flex gap-6">
-                    <Button class="bg-brand-blue text-lg transition-colors hover:bg-brand-blue-darker hover:shadow-lg">
+                    <Button class="bg-brand-blue text-lg transition-colors hover:bg-brand-blue-darker hover:shadow-lg dark:bg-brand-blue dark:text-brand-blue-foreground">
                         Get in touch
                     </Button>
                     <Button
